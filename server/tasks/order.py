0881aa9313926909198b8dc475ac711fbe3f2e46
maplethorpej <jeradmaplethorpe@gmail.com>
2017-02-05 21:22:27 -0600

    order product with proper SKUs

--- server/tasks/order.py
+++ server/tasks/order.py
@@ -8,6 +8,27 @@ def create_order(data):
         return 'Error: no token supplied.'
 
     customer = stripe.Customer.create(source=data['token'])
+
+    cus_info = customer['sources']['data'][0]
+    shipping = {
+        "name": cus_info['name'],
+        "address": {
+            "line1": cus_info['address_line1'],
+            "city": cus_info['address_city'],
+            "country": cus_info['address_country'],
+            "postal_code": cus_info['address_zip']
+        },
+    }
+
     items = data['items']
+    items_list = [{'type': 'sku', 'parent': sku} for sku in items]
+
+    stripe.Order.create(
+        currency='usd',
+        items=items_list,
+        customer=customer.id,
+        shipping=shipping,
+        email=data['email']
+    )
 
     return 'ok'
